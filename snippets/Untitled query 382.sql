cb4698112f2dbba5864904c72e74ac3f3adbad22
kchen <kchen@kchens-Mac-mini.local>
2026-03-03 23:29:47 -0800

    Stop tracking sensitive environment variables

--- snippets/Untitled query 382.sql
+++ snippets/Untitled query 382.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (auth_id, org_id, role_id, email, display_name, is_platform_admin)
 VALUES (
-  '44ccc5eb-1d2b-46ef-9e85-ca5d1ae7f544',
+  '54fc902e-64ba-4fd1-844e-d3b85f8c197f',
   '00000000-0000-0000-0000-000000000001',  -- Default HOA org
   '00000000-0000-0000-0000-000000000030',  -- Manager role
   'admin@yourdomain.com',
